8689c6d50c5005f252e84ada87557990c76a1e7f
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2020-07-09 18:36:06 -0400

    New Build

---
+++ build_app.bat
@@ -0,0 +1,8 @@
+cd C:\Users\micha\Documents\GitHub\personalWebsite
+REM ng build
+REM cp dist\personalWebsite1\* ..\Personal_Website_Build
+REM cd C:\Users\micha\Documents\GitHub\Personal_Website_Build
+echo "HELLLO"
+git add .
+git commit -m "New Build"
+git push
