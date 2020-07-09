a938641d88e10cd706fec1c7e916f24206ccd675
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2020-07-09 18:46:39 -0400

    Minor changes to HTML syntax

--- build_app.bat
+++ build_app.bat
@@ -1,8 +1,7 @@
 cd C:\Users\micha\Documents\GitHub\personalWebsite
 call ng build
-REM cp dist\personalWebsite1\* ..\Personal_Website_Build
-REM cd C:\Users\micha\Documents\GitHub\Personal_Website_Build
-echo "HELLLO"
+cp C:\Users\micha\Documents\GitHub\personalWebsite\dist\personalWebsite1\* C:\Users\micha\Documents\GitHub\Personal_Website_Build\
+cd C:\Users\micha\Documents\GitHub\Personal_Website_Build
 git add .
 git commit -m "New Build"
 git push
