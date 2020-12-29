ac53a838827186e65220f1224f87c28a15739ed9
Michael Krakovsky <michael_k26@hotmail.com>
2020-12-29 12:09:03 -0500

    New: Angular Build

--- build_app.bat
+++ build_app.bat
@@ -1,6 +1,6 @@
 cd C:\Users\micha\Documents\GitHub\personalWebsite
 call ng build
-cp C:\Users\micha\Documents\GitHub\personalWebsite\dist\personalWebsite1\* C:\Users\micha\Documents\GitHub\Personal_Website_Build\
+xcopy /s C:\Users\micha\Documents\GitHub\personalWebsite\dist\personalWebsite1 C:\Users\micha\Documents\GitHub\Personal_Website_Build
 cd C:\Users\micha\Documents\GitHub\Personal_Website_Build
 git add .
 git commit -m "New Build"
